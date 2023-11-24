4f21a075e2c725b764870cf558071bc8f456d6ba
stephanie jones <sjones.njones@gmail.com>
2023-11-24 14:14:01 -0600

    setting up server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../client/dist")))
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/app", (req, res) => {res.sendFile(path.join(__dirname, "../client/dist/index.html"))});
+
+app.listen(PORT, () => console.log(`Application is running on ${PORT}`));
