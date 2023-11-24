35834e409804092917b56f678e8701fa97764804
stephanie jones <sjones.njones@gmail.com>
2023-11-24 14:41:00 -0600

    updated and fixed vite config, updated server.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()], 
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      "/graphql": {
+        target: "http://localhost:3001",
+        changeOrgin: true,
+        secure: false
+      }
+    }
+  }
+  })
+
