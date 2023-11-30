bc45934bb34f976cc30c5f89b58af79828a89bc2
stephanie jones <sjones.njones@gmail.com>
2023-11-29 19:23:12 -0600

    added mongo user model and seeds

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Hodge-Knives
 A site for viewing and selling custom-made knives.
+
+sjonesnjones
+lhCkjG4VJTxAXlTB
--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
-
-
-
-
 const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const db = require("./config/connection")
+const { User } = require("./models")
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const {typeDefs, resolvers} = require("./schemas")
@@ -30,16 +27,13 @@ const startApolloServer = async () => {
       res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
   }
+  
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`Application is running on ${PORT}!`);
+    });
+  });
 }
-
-db.once("open", () => {
-
-  app.listen(PORT, () => {
-    console.log(`Application is running on ${PORT}`)
-  }
-  )
-}
-);
 
 startApolloServer();
 
