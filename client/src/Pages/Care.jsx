189a7ac10923ad66a7c14980d057eaa3e3b5fb69
stephanie jones <sjones.njones@gmail.com>
2023-12-17 23:52:38 -0600

    care page fixed up

--- client/src/Pages/Care.jsx
+++ client/src/Pages/Care.jsx
@@ -1,15 +1,44 @@
-import Maker from '../assets/images/makerPic.png';
 const Care = () => {
 
   return (
     <div>
       <h1 className='m-3 text-white'>How to Care for Your Knife</h1>
-      <div className='card bg-black'>
-       
-      <p className='text-white text-justify m-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi harum, obcaecati voluptatibus ipsum delectus accusantium, autem, voluptas velit aperiam officiis quod minima. Minus corrupti odit laboriosam perferendis. Tempora, voluptatibus dolore.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem, alias. Vel accusamus et nihil temporibus, sequi harum aspernatur voluptate rem aperiam ducimus molestiae deleniti, repellat, maxime atque magni perspiciatis pariatur? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore omnis reprehenderit laudantium, modi est facere accusamus minima magnam incidunt aut exercitationem non quas saepe, amet porro delectus sequi optio fugit?
-      </p>
-      <p className='text-white text-justify m-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi harum, obcaecati voluptatibus ipsum delectus accusantium, autem, voluptas velit aperiam officiis quod minima. Minus corrupti odit laboriosam perferendis. Tempora, voluptatibus dolore.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem, alias. Vel accusamus et nihil temporibus, sequi harum aspernatur voluptate rem aperiam ducimus molestiae deleniti, repellat, maxime atque magni perspiciatis pariatur? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore omnis reprehenderit laudantium, modi est facere accusamus minima magnam incidunt aut exercitationem non quas saepe, amet porro delectus sequi optio fugit?</p>
-      <p className='text-white text-justify m-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi harum, obcaecati voluptatibus ipsum delectus accusantium, autem, voluptas velit aperiam officiis quod minima. Minus corrupti odit laboriosam perferendis. Tempora, voluptatibus dolore.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem, alias. Vel accusamus et nihil temporibus, sequi harum aspernatur voluptate rem aperiam ducimus molestiae deleniti, repellat, maxime atque magni perspiciatis pariatur? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore omnis reprehenderit laudantium, modi est facere accusamus minima magnam incidunt aut exercitationem non quas saepe, amet porro delectus sequi optio fugit?</p>
+      <div className='mx-auto text-start w-75 card bg-black'>
+
+        <h3 className='mb-4 fw-bold text-white'> 1. Don’t leave your knives in the kitchen sink.</h3>
+        <h4 className="text-white fw-light">
+          Not only is it dangerous for whoever washes the dishes, it’s also bad for your knives — the blade can get scratched, or worse, the tip can bend or break. As soon as you’re finished using your knife, wash it, dry it and put it away in safe place.
+        </h4>
+        <h3 className='my-4 fw-bold text-white'>2. Don’t store your knives in the utensil drawer.</h3>
+        <h4 className="text-white fw-light">
+          Throwing your knives in any drawer, mixed in with other utensils, is one of the worst things you can do. The blade can easily get scratched and dented from being jostled around in the drawer every time you open it. If you don’t have any other place to store your knives at least use a knife sheath to guard the blade.
+        </h4>
+        <h3 className='my-4 fw-bold text-white'>
+          3. Always wash your knives by hand.
+        </h3>
+        <h4 className="text-white fw-light">
+          The dishwasher might be convenient, but there’s a high risk that the blade will get dinged during the wash cycle. Wash your knives by hand and the blade will thank you.
+        </h4>
+        <h3 className='my-4 fw-bold text-white'>
+          4. Don’t leave your knives to dry in the dish rack.
+        </h3>
+        <h4 className="text-white fw-light">
+          You run the risk of dulling the blade when the knife shares space with other utensils in the utensil bin. Dry your knife immediately after washing, to prevent any mold or mildew from forming, and put it away.
+        </h4>
+        <h3 className='my-4 fw-bold text-white'>
+          5. Sharpen the blade.
+        </h3>
+        <h4 className="text-white fw-light">
+          It’s like a tune-up for the knife. There are many ways to do this: using a honing steel, a knife stone, or even send it out to a me!
+        </h4>
+        <h3 className='my-4 fw-bold text-white'>
+        6. Always cut on cutting boards.</h3>
+        <h4 className="text-white fw-light">
+        Don’t cut on your countertop — just don’t do it. Marble, granite or any solid surface is too hard for the blade. Stick with wood cutting boards, they’re the gentlest.
+        </h4>
+        <h4 className="text-white fw-light">
+        Basically, use it, wash it, dry it, store it. If you follow those steps, your knife will prove to be worth every penny.
+        </h4>
       </div>
     </div>
   );
