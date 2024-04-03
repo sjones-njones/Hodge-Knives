0cb10a3c3671b0eebc032975d1656f991ab8d1f7
stephanie jones <sjones.njones@gmail.com>
2024-04-03 15:38:49 -0500

    fixed button sizing

--- client/src/Pages/Admin.jsx
+++ client/src/Pages/Admin.jsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom';
-
+ 
 const Admin = () => {
     return (
       <div className="m-4">
         <Link to="/Admin/AdminCurrentInventory"> 
-<button type="button" className="fs-1 m-2 btn btn-warning btn-block">Current Inventory</button> 
+<button type="button" className="fs-1 sizingButton m-2 btn btn-warning btn-block">Current Inventory</button> 
         </Link>
 <Link to="/Admin/AdminArchiveInventory"> 
-<button type="button" className="fs-1 m-2 btn btn-warning btn-block">Archived Inventory</button> 
+<button type="button" className="fs-1 m-2 btn sizingButton btn-warning btn-block">Archived Inventory</button> 
         </Link>
 <Link to="/Admin/AddProducts"> 
-<button type="button" className="m-2 fs-1 btn btn-warning btn-block">Add New Products</button> 
+<button type="button" className="m-2 fs-1 btn sizingButton btn-warning btn-block">Add New Products</button> 
 </Link>
 
      </div>
